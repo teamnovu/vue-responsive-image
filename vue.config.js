4e2b39958c896b2ed718e5c85137500d4778246f
Tom Diggelmann <mail@tomdgm.ch>
2020-04-23 00:58:12 +0200

    docs: fix gh-pages demo public path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.DEPLOY_ENV === 'GH_PAGES' ? 'vue-responsive-image/' : '/'
+  publicPath: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/vue-responsive-image/' : '/'
 }
